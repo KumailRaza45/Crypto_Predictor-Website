504bd5c2835e01b988918f1d7434cbea3561f3b8
KumailRaza45 <103637990+KumailRaza45@users.noreply.github.com>
2022-05-29 15:18:40 +0500

    Commit

--- crypto/crypto/urls.py
+++ crypto/crypto/urls.py
@@ -18,10 +18,14 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('crypto/', include('crypto_app.urls')),
-     path('accounts/', include('Accounts.urls')),
+    path('accounts/', include('Accounts.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
